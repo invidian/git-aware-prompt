8695d7d5a2785ee13f69a71fe92661cc11086392
Mateusz Gozdek <mgozdekof@gmail.com>
2020-08-14 17:38:47 +0200

    prompt.sh: make git prompt compatible with my prompt
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- prompt.sh
+++ prompt.sh
@@ -1,4 +1,5 @@
 find_git_branch() {
+  export Last_Command=$?
   # Based on: http://stackoverflow.com/a/13003854/170413
   local branch
   if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
